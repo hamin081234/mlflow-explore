f93af640135da9735eaadcc2ce27e630ef1bf834
천하민 <hamin081234@gmail.com>
2024-03-28 15:36:44 +0900

    [UPDATE] inference_class added and can be served with gpu

--- bentoml/donut_bentoml.py
+++ bentoml/donut_bentoml.py
@@ -1,8 +1,11 @@
 import bentoml
-from transformers import VisionEncoderDecoderModel, DonutProcessor
+from mlflow_manager import load_transformers_donut
+# from transformers import VisionEncoderDecoderModel, DonutProcessor
 
-processor = DonutProcessor.from_pretrained("HamAndCheese82/math_ocr_donut_v1")
-model = VisionEncoderDecoderModel.from_pretrained("HamAndCheese82/math_ocr_donut_v1")
+# processor = DonutProcessor.from_pretrained("HamAndCheese82/math_ocr_donut_v1")
+# model = VisionEncoderDecoderModel.from_pretrained("HamAndCheese82/math_ocr_donut_v1")
+
+model, processor = load_transformers_donut("donut_transformers", model_version=1)
 
 bentoml.transformers.save_model("donut_processor", processor)
 bentoml.transformers.save_model("donut_model", model)
--- bentoml/service.py
+++ bentoml/service.py
@@ -4,62 +4,36 @@ from PIL import Image as PILImage
 import re
 from time import time
 import io
+import torch
+from inference import InferenceClass
 
 processor_ref = bentoml.transformers.get("donut_processor:latest")
 model_ref = bentoml.transformers.get("donut_model:latest")
 
 class DonutRunnable(bentoml.Runnable):
-    SUPPORTED_RESOURCES = ('nvidia.com/gp', 'cpu')
+    SUPPORTED_RESOURCES = ('nvidia.com/gpu', 'cpu')
     SUPPORTS_CPU_MULTI_THREADING = True
 
     def __init__(self):
         self.processor = bentoml.transformers.load_model(processor_ref)
         self.model = bentoml.transformers.load_model(model_ref)
-        # self.embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
-        # self.speaker_embeddings = torch.tensor(self.embeddings_dataset[7306]["xvector"]).unsqueeze(0)
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        self.model.to(self.device)
+
+        self.inferece_class = InferenceClass(self.model, self.processor, self.device)
 
     @bentoml.Runnable.method(batchable=False)
     def generate_latex(self, inp: Image):
+        print(self.device)
+
         inp = inp.convert("RGB")
-        pixel_values = self.processor(inp, return_tensors='pt').pixel_values
+        result = self.inferece_class.inference(inp)
 
-        decoder_input_ids = self.processor.tokenizer(
-            "<MathOCR>",
-            add_special_tokens=False,
-            return_tensors="pt"
-        )["input_ids"]
-        
-        start_time = time()
-        outputs = self.model.generate(
-            pixel_values,
-            decoder_input_ids=decoder_input_ids,
-            max_length=self.model.config.decoder.max_position_embeddings,
-            pad_token_id=self.processor.tokenizer.pad_token_id,
-            eos_token_id=self.processor.tokenizer.eos_token_id,
-            use_cache=True,
-            num_beams=1,
-            bad_words_ids=[[self.processor.tokenizer.unk_token_id]],
-            return_dict_in_generate=True,
-            output_scores=True,
-        )
-        dur = time()-start_time
-
-        decoded_sequences = self.processor.batch_decode(outputs.sequences, skip_special_tokens=True)
-
-        pred_class_list = []
-        latex_list = []
-        for output in decoded_sequences:
-            pred_class = 'HW' if re.findall(r'<.*?>', output)[1] == '<s_HW>' else 'P'
-            pred_class_list.append(pred_class)
-
-            special_tokens = self.processor.tokenizer.added_tokens_decoder
-            spc_rm = '|'.join([str(special_tokens[key]) for key in special_tokens.keys()])
-            latex = re.sub(r"{}".format(spc_rm), " ", output).strip()
-            latex_list.append(latex)
-
-        return {'latex': latex_list[0], 'duration': dur}
+        return result
 
 donut_runner = bentoml.Runner(DonutRunnable, name="donut_runner", models=[processor_ref, model_ref])
+
 svc = bentoml.Service("image2latex", runners=[donut_runner])
 
 @svc.api(input=File(), output=JSON())
